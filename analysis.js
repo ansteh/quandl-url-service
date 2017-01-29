afa85b9164c69315b7fd1eb4585061d7b22b6eac
ansteh <stehle.andre@googlemail.com>
2017-01-29 22:45:14 +0100

    get start dates

--- analysis.js
+++ analysis.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const urls = require('./lib/url-generator.js');
 const util = require('./lib/util.js');
 const Promise = require('bluebird');
+const moment = require('moment');
 
 const Stock = (stock) => {
   let dataset = stock.dataset;
@@ -11,18 +12,29 @@ const Stock = (stock) => {
 
   const getIndicesOfNeedles = getNeedlesIndicesOfColumns(dataset.column_names);
   // console.log(_.keys(dataset));
-  // console.log(dataset.column_names);
+  // console.log(_.keys(dataset));
   // console.log(_.first(data));
 
-  const getData = (columns) => {
+  const getData = (columns, start, end) => {
     let indices = getIndicesOfNeedles(columns);
     return _.map(data, (set) => {
       return _.map(indices, index => set[index]);
     });
   };
 
+  const getStartDate = () => {
+    console.log(dataset.start_date);
+    return _.get(dataset, 'start_date');
+  };
+
+  const getEndDate = () => {
+    return _.get(dataset, 'end_date');
+  };
+
   return {
-    getData
+    getData,
+    getStartDate,
+    getEndDate
   };
 };
 
@@ -31,17 +43,17 @@ const getNeedlesIndicesOfColumns = _.curry((columns, needles) => {
 });
 
 const Stocks = (metas) => {
-  const cache = new Map();
+  const cache = {};
 
   const getStock = (ticker) => {
     return new Promise((resolve, reject) => {
-      if(cache.has(ticker)) {
-        resolve(cache.get(ticker));
+      if(_.has(cache, ticker)) {
+        resolve(_.get(cache, ticker));
       } else {
         util.loadFileContent(`${__dirname}/resources/cached/${ticker}.json`)
           .then((jsonStr) => {
             let stock = Stock(JSON.parse(jsonStr));
-            cache.set(ticker, stock);
+            _.set(cache, ticker, stock);
             resolve(stock);
           })
           .catch(reject);
@@ -49,13 +61,26 @@ const Stocks = (metas) => {
     });
   };
 
-  // Promise.all(_.map(metas, meta => getStock(meta.ticker)))
-  // .then(function() {
-  //   console.log("all the files were created", cache.size);
-  // });
+  const getStarDates = (stocks) => {
+    return _.map(stocks, stock => moment(stock.getStartDate(), 'YYYY-MM-DD'));
+  };
+
+  const getEndDates = (stocks) => {
+    return _.map(stocks, stock => moment(stock.getEndDate(), 'YYYY-MM-DD'));
+  };
+
+  Promise.all(_.map(_.take(metas, 10), meta => getStock(meta.ticker)))
+  .then(function() {
+    console.log("all the files were created");
+    let stocks = _.values(cache);
+    let startDates = getStarDates(stocks);
+    let endDates = getEndDates(stocks);
+    console.log(_.max(startDates));
+    console.log(_.min(endDates));
+  });
 
   return {
-    getStock: getStock
+    getStock
   };
 };
 
