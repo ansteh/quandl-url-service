67113f572b2913a3721a723b196e487824c4b751
ansteh <stehle.andre@googlemail.com>
2017-02-16 23:41:10 +0100

    data lab

--- labs/viz.py
+++ labs/viz.py
@@ -5,17 +5,26 @@ import json
 import matplotlib
 import matplotlib.pyplot as p
 
-with open('resources/aggregation/SP500/close.json') as data_file:
-    # print(ps.read_json(content, orient='values'))
-    data = json.load(data_file)
-    dates = pd.to_datetime(data['dates'], format="%Y-%m-%d")
-    dataset = np.array(data['dataset'], dtype=np.float16)
-    # print(dates.shape)
-    # print(dataset[0][0])
+def get_data(filename):
+    with open(filename) as data_file:
+        # print(ps.read_json(content, orient='values'))
+        data = json.load(data_file)
+        dates = pd.to_datetime(data['dates'], format="%Y-%m-%d")
+        dataset = np.array(data['dataset'], dtype=np.float16)
+        # print(dates.shape)
+        # print(dataset[0][0])
 
-    column_non_zeros = np.apply_along_axis(np.count_nonzero, 0, dataset)
-    closes = np.sum(dataset, axis=0)/column_non_zeros
+        return dates, dataset
 
-    df = pd.DataFrame({ 'date' : dates, 'close' : closes })
-    df.plot(x='date', y='close')
-    p.show()
+dates, dataset = get_data('resources/aggregation/SP500/close.json')
+
+column_non_zeros = np.apply_along_axis(np.count_nonzero, 0, dataset)
+closes = np.sum(dataset, axis=0)/column_non_zeros
+
+df = pd.DataFrame({ 'date' : dates, 'close' : closes })
+df.plot(x='date', y='close')
+
+df = pd.DataFrame({ 'date' : dates, 'count' : column_non_zeros })
+df.plot(x='date', y='count')
+
+p.show()
