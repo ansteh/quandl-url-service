8971199f12e7be9d3bd3196a9415b0f54ed9d73f
ansteh <stehle.andre@googlemail.com>
2017-03-23 22:15:33 +0100

    parse daily links

--- testbed/index.js
+++ testbed/index.js
@@ -1,12 +1,30 @@
 'use strict';
-const util = require('../lib/util.js');
-const _    = require('lodash');
+const util   = require('../lib/util.js');
+const _      = require('lodash');
+const fs     = require('fs');
+const moment = require('moment');
 
-const parse = () => {
-  return util.loadFileContent(`${__dirname}/test.txt`)
+const parse = (filename) => {
+  let dateStr = _.replace(filename, '.txt', '');
+  let date = moment(dateStr, 'DD-MM-YYYY').toDate();
+
+  return util.loadFileContent(`${__dirname}/daily-links/${filename}`)
   .then(content => _.dropRight(_.split(content, '\r\n')))
-  .then(links => console.log(links.length))
-  .catch(console.log);
+  .then(links => _.map(links, (link) => {
+    return { date, link };
+  }));
 };
 
-parse();
+const getFiles = (folder) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(folder, (err, files) => {
+      err ? reject(err) : resolve(files);
+    })
+  });
+};
+
+getFiles(`${__dirname}/daily-links`)
+.then(files => Promise.all(_.map(files, parse)))
+.then(batches => _.flatten(batches))
+.then(links => console.log(links.length))
+.catch(console.log);
